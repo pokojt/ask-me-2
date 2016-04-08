40be471a0ebacfd8d7eb0e477c38031caec1a507
Taylor Pokoj <pokojt@gmail.com>
2016-04-08 08:30:52 -0700

    Add date to answer tile

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -6,10 +6,14 @@ export default Ember.Component.extend({
       this.toggleProperty('answerFormShowing');
     },
     saveNewAnswer() {
+      var date = new Date();
+      date = date.toString().substring(4, 15);
+      var momentDate = moment(date).format('MM/DD/YYYY');
       var formInputs = {
         author: this.get('author') ? this.get('author'):"",
         content: this.get('content') ? this.get('content'):"",
         question: this.get('question'),
+        date: momentDate,
       };
       this.toggleProperty('answerFormShowing');
       this.set('author', "");
