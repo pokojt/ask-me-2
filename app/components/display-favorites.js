a4ff30ff80e4e1723899f71e02b92d2d40422324
Taylor Pokoj <pokojt@gmail.com>
2016-04-08 09:53:28 -0700

    Add functionality to adding a question to favorites

--- app/components/display-favorites.js
+++ app/components/display-favorites.js
@@ -2,4 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   myFavorites: Ember.inject.service(),
+
+  actions: {
+    removeFromFavorites(favorite) {
+      if(confirm("Are you sure you want to remove this from your favorites?")) {
+        this.sendAction('removeFromFavorites', favorite);
+      }
+    }
+  }
 });
--- app/routes/favorites.js
+++ app/routes/favorites.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  myFavorites: Ember.inject.service(),
+
+  actions: {
+    removeFromFavorites(favorite) {
+      this.get('myFavorites').remove(favorite);
+      this.refresh('myFavorites.favorites');
+    }
+  }
 });
